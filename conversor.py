5d639957c0b7c970b5fcb853d033984649e411b9
esjavierml <83562418+esjavierml@users.noreply.github.com>
2021-05-18 01:58:25 -0600

    Utilizando la instrucción if

--- conversor.py
+++ conversor.py
@@ -1,7 +1,38 @@
-pesos = input("¿cuantos pesos tiene?: ")
-pesos = float(pesos)
-valor_dolar = 3875 
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares")
+menu = """
+Bienvenido al conversor de monedas 💰
+1 - Pesos colombianos
+2 - Pesos mexicanos
+3 - Pesos argentinos
+
+Elige una opción: """
+opcion = input(menu)
+
+if opcion == "1":
+    pesos = input("¿cuantos pesos colombianos tiene?: ")
+    pesos = float(pesos)
+    valor_dolar = 3674 
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+
+elif opcion == "2":
+    pesos = input("¿cuantos pesos mexicanos tiene?: ")
+    pesos = float(pesos)
+    valor_dolar = 19.78 
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+
+elif opcion == "3":
+    pesos = input("¿cuantos pesos argentinos tiene?: ")
+    pesos = float(pesos)
+    valor_dolar = 94.04
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+
+else:
+    print("ingresa una opción coreecta")
